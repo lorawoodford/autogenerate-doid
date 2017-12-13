ea1291666c9ee73053520d847ee1e3788c0aa071
Lora Woodford <lorawoodford@gmail.com>
2017-12-13 12:44:24 -0500

    make 1.5.4 compatible

--- backend/model/digital_object.rb
+++ backend/model/digital_object.rb
@@ -21,7 +21,6 @@ class DigitalObject < Sequel::Model(:digital_object)
   include ComponentsAddChildren
   include Events
   include Publishable
-  include Assessments::LinkedRecord
 
   enable_suppression
 
